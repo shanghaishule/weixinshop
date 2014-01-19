6832a861bded3e01062becacf48463fcbb57f567
polo2013 <paul.zhou@163.com>
2014-01-19 15:22:26 +0800

    修改db-config

--- SRC/weTall/data/config/db.php
+++ SRC/weTall/data/config/db.php
@@ -1,9 +1,9 @@
 <?php
 return $arr= array (
   'DB_HOST' => 'localhost',
-  'DB_NAME' => 'bestchoi_shule',//数据库名
-  'DB_USER' => 'bestchoi_shule',//数据库用户,一般不用改	
-  'DB_PWD' => 'shule_123',      //数据库密码
+  'DB_NAME' => 'bestchoi_shule',
+  'DB_USER' => 'bestchoi_shule',
+  'DB_PWD' => 'shule_123',
   'DB_PORT' => '3306',
   'DB_PREFIX' => 'tp_weixin_',
 );
