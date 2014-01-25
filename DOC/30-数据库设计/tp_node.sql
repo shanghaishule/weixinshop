f35e6d05c0e269030c2bd89ec3a5bf3c4c72a690
greysonzhou <zhouxingxing_ok_12@163.com>
2014-01-25 15:12:49 +0800

    aaaaaa

---
+++ DOC/30-数据库设计/tp_node.sql
@@ -0,0 +1,139 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.4
+-- http://www.phpmyadmin.net
+--
+-- 主机: localhost
+-- 生成日期: 2014 年 01 月 25 日 07:08
+-- 服务器版本: 5.6.12-log
+-- PHP 版本: 5.4.16
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- 数据库: `bestchoi_shule`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `tp_node`
+--
+
+CREATE TABLE IF NOT EXISTS `tp_node` (
+  `id` smallint(6) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL COMMENT '节点名称',
+  `title` varchar(50) NOT NULL COMMENT '菜单名称',
+  `status` tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否激活 1：是 2：否',
+  `remark` varchar(255) DEFAULT NULL COMMENT '备注说明',
+  `pid` smallint(6) unsigned NOT NULL COMMENT '父ID',
+  `level` tinyint(1) unsigned NOT NULL COMMENT '节点等级',
+  `data` varchar(255) DEFAULT NULL COMMENT '附加参数',
+  `sort` smallint(6) unsigned NOT NULL DEFAULT '0' COMMENT '排序权重',
+  `display` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '菜单显示类型 0:不显示 1:导航菜单 2:左侧菜单',
+  PRIMARY KEY (`id`),
+  KEY `level` (`level`),
+  KEY `pid` (`pid`),
+  KEY `status` (`status`),
+  KEY `name` (`name`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=91 ;
+
+--
+-- 转存表中的数据 `tp_node`
+--
+
+INSERT INTO `tp_node` (`id`, `name`, `title`, `status`, `remark`, `pid`, `level`, `data`, `sort`, `display`) VALUES
+(1, 'cms', '根节点', 1, '', 0, 1, NULL, 0, 0),
+(2, 'Site', '站点管理', 1, '', 1, 0, NULL, 0, 1),
+(3, 'User', '用户管理', 1, '', 1, 0, NULL, 0, 1),
+(4, 'extent', '扩展管理', 1, '', 1, 0, NULL, 10, 1),
+(5, 'article', '内容管理', 1, '', 1, 0, NULL, 0, 1),
+(6, 'Site', '站点设置', 1, '', 2, 2, NULL, 0, 2),
+(7, 'index', '基本信息设置', 1, '', 6, 3, NULL, 0, 2),
+(8, 'safe', '安全设置', 1, '', 6, 3, NULL, 0, 2),
+(9, 'email', '邮箱设置', 1, '', 6, 3, NULL, 0, 2),
+(10, 'upfile', '附件设置', 1, '', 6, 3, NULL, 0, 2),
+(11, 'Node', '节点管理', 1, NULL, 2, 2, NULL, 0, 2),
+(12, 'add', '添加节点', 1, '', 11, 3, NULL, 0, 2),
+(13, 'index', '节点列表', 1, '', 11, 3, NULL, 0, 2),
+(14, 'insert', '写入', 1, '0', 11, 3, NULL, 0, 0),
+(15, 'edit', '编辑节点', 1, '0', 11, 3, NULL, 0, 0),
+(16, 'update', '更新节点', 1, '0', 11, 3, NULL, 0, 0),
+(17, 'del', '删除节点', 1, '0', 11, 3, NULL, 0, 0),
+(18, 'User', '用户中心', 1, '0', 3, 2, NULL, 0, 2),
+(19, 'add', '添加用户', 1, '0', 18, 3, NULL, 0, 2),
+(20, 'index', '用户列表', 1, '0', 18, 3, NULL, 0, 2),
+(21, 'edit', '编辑用户', 1, '0', 18, 3, NULL, 0, 0),
+(22, 'insert', '写入数据库', 1, '0', 18, 3, NULL, 0, 0),
+(23, 'update', '更新用户', 1, '0', 18, 3, NULL, 0, 0),
+(24, 'del', '删除用户', 1, '0', 18, 3, NULL, 0, 0),
+(25, 'Group', '管理组', 1, '0', 3, 2, NULL, 0, 2),
+(26, 'add', '创建用户组', 1, '0', 25, 3, NULL, 0, 2),
+(27, 'index', '用户组列表', 1, '0', 25, 3, NULL, 0, 2),
+(28, 'edit', '编辑用户组', 1, '0', 25, 3, NULL, 0, 0),
+(29, 'del', '删除用户组', 1, '0', 25, 3, NULL, 0, 0),
+(30, 'insert', '写入数据库', 1, '0', 25, 3, NULL, 0, 0),
+(31, 'update', '更新用户组', 1, '0', 25, 3, NULL, 0, 0),
+(32, 'insert', '保存测试', 1, '0', 6, 3, NULL, 0, 0),
+(36, 'menu', '左侧栏', 1, '0', 35, 3, NULL, 0, 0),
+(35, 'System', '首页', 1, '0', 1, 0, NULL, 0, 0),
+(37, 'main', '右侧栏目', 1, '0', 35, 3, NULL, 0, 0),
+(38, 'Article', '微信图文', 1, '0', 5, 2, NULL, 0, 2),
+(39, 'index', '图文列表', 1, '0', 38, 3, NULL, 0, 2),
+(40, 'add', '图文添加', 1, '0', 38, 3, NULL, 0, 2),
+(41, 'edit', '微信图文编辑', 1, '0', 38, 3, NULL, 0, 0),
+(42, 'del', '微信图文删除', 1, '0', 38, 3, NULL, 0, 0),
+(80, 'token', '公众号管理', 1, '0', 1, 1, NULL, 0, 1),
+(45, 'Function', '功能模块', 1, '0', 1, 0, NULL, 0, 1),
+(46, 'Function', '功能模块', 1, '0', 45, 2, NULL, 0, 2),
+(47, 'add', '添加模块', 1, '0', 46, 3, NULL, 0, 2),
+(48, 'User_group', '会员组', 1, '0', 3, 2, NULL, 0, 2),
+(49, 'add', '添加会员组', 1, '0', 48, 3, NULL, 0, 2),
+(50, 'Users', '前台用户', 1, '0', 3, 2, NULL, 0, 2),
+(51, 'index', '用户列表', 1, '0', 50, 3, NULL, 0, 0),
+(52, 'add', '添加用户', 1, '0', 50, 3, NULL, 0, 2),
+(53, 'edit', '编辑用户', 1, '0', 50, 3, NULL, 0, 0),
+(54, 'del', '删除用户', 1, '0', 50, 3, NULL, 0, 0),
+(55, 'insert', '写入数据库', 1, '0', 50, 3, NULL, 0, 0),
+(56, 'upsave', '更新用户', 1, '0', 50, 3, NULL, 0, 0),
+(57, 'Text', '微信文本', 1, '0', 5, 2, NULL, 0, 2),
+(58, 'index', '文本列表', 1, '0', 57, 3, NULL, 0, 2),
+(59, 'del', '删除', 1, '0', 57, 3, NULL, 0, 0),
+(60, 'Custom', '自定义页面', 1, '0', 5, 2, NULL, 0, 2),
+(61, 'index', '列表', 1, '0', 60, 3, NULL, 0, 2),
+(62, 'edit', '编辑', 1, '0', 60, 3, NULL, 0, 0),
+(63, 'del', '删除', 1, '0', 60, 3, NULL, 0, 0),
+(64, 'Records', '充值记录', 1, '0', 4, 2, NULL, 0, 2),
+(65, 'index', '充值列表', 1, '0', 64, 3, NULL, 0, 2),
+(66, 'Case', '用户案例', 1, '0', 4, 2, NULL, 0, 2),
+(67, 'index', '案例列表', 1, '0', 66, 3, NULL, 0, 2),
+(68, 'add', '添加案例', 1, '0', 66, 3, NULL, 0, 2),
+(69, 'edit', '编辑案例', 1, '0', 66, 3, NULL, 0, 0),
+(70, 'del', '删除案例', 1, '0', 66, 3, NULL, 0, 0),
+(71, 'insert', '写入数据库', 1, '0', 66, 3, NULL, 0, 0),
+(72, 'upsave', '更新数据库', 1, '0', 66, 3, NULL, 0, 0),
+(73, 'Links', '友情链接', 1, '0', 4, 2, NULL, 0, 2),
+(74, 'index', '友情链接', 1, '0', 73, 3, NULL, 0, 2),
+(75, 'add', '添加链接', 1, '0', 73, 3, NULL, 0, 2),
+(76, 'edit', '编辑链接', 1, '0', 73, 3, NULL, 0, 0),
+(77, 'insert', '插入数据库', 1, '0', 73, 3, NULL, 0, 0),
+(78, 'upsave', '更新数据库', 1, '0', 73, 3, NULL, 0, 0),
+(79, 'del', '删除友情链接', 1, '0', 73, 3, NULL, 0, 0),
+(81, 'Token', '公众号管理', 1, '0', 80, 2, NULL, 0, 2),
+(83, 'alipay', '在线支付接口', 1, '0', 6, 3, NULL, 0, 2),
+(84, 'Tall', '商城管理', 1, '0', 1, 0, NULL, 0, 1),
+(87, 'Brand', '品牌管理', 1, '品牌管理', 84, 2, NULL, 0, 2),
+(86, 'Tall', '商品分类', 1, '0', 84, 2, NULL, 0, 2),
+(88, 'Info_Notice', '信息发布管理', 1, '公告发布管理', 84, 2, NULL, 0, 2),
+(89, 'AdNTAX', '广告及汇率设置', 1, '0', 84, 2, NULL, 0, 2),
+(90, 'Statistics', '商家结算系统', 1, '0', 84, 2, NULL, 0, 2);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
