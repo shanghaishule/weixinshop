f422e9d45e58a79ad1b7b7d5e6c70cad5024e4ce
polo2013 <paul.zhou@163.com>
2014-02-09 18:09:59 +0800

    优化微指购商城的后台

--- SRC/data/conf/info.php
+++ SRC/data/conf/info.php
@@ -1,16 +1,16 @@
 <?php 
 return array (
-  'site_name' => '微信微信营销专家',
-  'site_title' => '微信营销专家',
-  'site_url' => 'localhost:8081',
-  'site_my' => 'cccc',
+  'site_name' => '微指购',
+  'site_title' => '微指购',
+  'site_url' => 'localhost',
+  'site_my' => '微指购',
   'ischeckuser' => 'true',
-  'ipc' => '',
+  'ipc' => '微指购',
   'site_qq' => '404249022',
   'baidu_map_api' => '2b79c282b0f365ca9782cceb499e6022',
   'site_email' => 'service@555.com',
-  'keyword' => '微信',
-  'content' => '微信营销专家',
+  'keyword' => '微指购',
+  'content' => '微指购',
   'counts' => '',
   'copyright' => '',
 );
