ad3b74b89666397b1618ead22c571d4222fe2a6d
greysonzhou <zhouxingxing_ok_12@163.com>
2014-02-10 18:30:22 +0800

    昨天的美化工作

--- SRC/Lanrain/Lib/Action/Wap/weTallAction.class.php
+++ SRC/Lanrain/Lib/Action/Wap/weTallAction.class.php
@@ -1,12 +1,28 @@
 <?php
   class weTallAction extends BaseAction{
     public function index(){
+    	//广告位
     	$weTallad = M("adforhome");
+    	//所有品牌
+    	$broadlist=M("brandlist")->select();
+    	//一级菜单
+    	$iteml["pid"]="0";
+    	$item_l1=M("item_cate")->where($iteml)->select();
+    	
+    	//二级菜单	
+    	$iteml2["pid"]=array("NEQ","0");
+    	$item_l2=M("item_cate")->where($iteml2)->select();
+    	
     	$data["status"]=1;
     	$data["checkstatus"]=1;
     	$data["boadid"]=array(array('eq',1),array('eq',2),array('eq',3),'or');
     	$weTallboard = $weTallad->where($data)->select();
-    	$this->assign("weTallboard",$weTallboard);echo "hi";
+    	
+    	$this->assign("weTallboard",$weTallboard);
+    	$this->assign("brandlist",$broadlist);
+    	$this->assign("item1",$item_l1);
+    	$this->assign("item",$item_l1);
+    	$this->assign("itemtwo",$item_l2);
         $this->display();
     }
   }
