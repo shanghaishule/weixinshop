6efc4f560ff9819d75f70dc68935ecfc2bf94d7a
greysonzhou <zhouxingxing_ok_12@163.com>
2014-02-13 15:44:45 +0800

    分享功能，店铺logo设置，店铺二维码设置以及预览的修改！

--- SRC/weTall/app/Lib/Action/admin/settingAction.class.php
+++ SRC/weTall/app/Lib/Action/admin/settingAction.class.php
@@ -77,7 +77,53 @@ class settingAction extends backendAction {
 //             $val = is_array($val) ? serialize($val) : $val;
 //             $this->_mod->where(array('name' => $key))->save(array('data' => $val));
 //         }
-        $data=$_POST;
+
+    	if(IS_POST){
+	        $data=$_POST;
+	        if (!empty($_FILES['twodcode']['name'])) {	        	
+	        
+		        //上传图片
+		        $date_dir = date('ym/d/'); //上传目录
+		        $item_headurls = array(); //相册
+		        $result = $this->_upload($_FILES['twodcode'], 'item/'.$date_dir, array(
+		        		'width'=>C('pin_item_bimg.width').','.C('pin_item_img.width').','.C('pin_item_simg.width'),
+		        		'height'=>C('pin_item_bimg.height').','.C('pin_item_img.height').','.C('pin_item_simg.height'),
+		        		'suffix' => '_b,_m,_s',
+		        		//'remove_origin'=>true
+		        ));
+		        if ($result['error']) {
+		        	$this->error($result['info']);
+		        } else {
+		        	$data['twodcode'] = $date_dir . $result['info'][0]['savename'];
+		        	//保存一份到相册
+		        	$item_imgs[] = array(
+		        			'twodcode'     => $data['twodcode'],
+		        	);
+		        }
+		        $data['twodcode'] = "/weTall/data/upload/item/".$data['twodcode'];
+	        }
+	        if (!empty($_FILES['logo']['name'])) {
+		        //上传图片
+		        $date_dir = date('ym/d/'); //上传目录
+		        $item_headurls = array(); //相册
+		        $result = $this->_upload($_FILES['logo'], 'item/'.$date_dir, array(
+		        		'width'=>C('pin_item_bimg.width').','.C('pin_item_img.width').','.C('pin_item_simg.width'),
+		        		'height'=>C('pin_item_bimg.height').','.C('pin_item_img.height').','.C('pin_item_simg.height'),
+		        		'suffix' => '_b,_m,_s',
+		        		//'remove_origin'=>true
+		        ));
+		        if ($result['error']) {
+		        	$this->error($result['info']);
+		        } else {
+		        	$data['logo'] = $date_dir . $result['info'][0]['savename'];
+		        	//保存一份到相册
+		        	$item_imgs[] = array(
+		        			'logo'     => $data['logo'],
+		        	);
+		        }
+		        $data['logo'] = "/weTall/data/upload/item/".$data['logo'];
+	        }
+    	}
         $weChaShop = M("wecha_shop");
         
         $wechaData1["tokenTall"] = $_SESSION["tokenTall"];
