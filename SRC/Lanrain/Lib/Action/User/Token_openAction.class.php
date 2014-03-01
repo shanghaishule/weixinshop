f8eea83c623b0574b1da63828e83789bf21d6686
polo2013 <paul.zhou@163.com>
2014-03-01 18:50:17 +0800

    默认给用户勾选所有功能

--- SRC/Lanrain/Lib/Action/User/Token_openAction.class.php
+++ SRC/Lanrain/Lib/Action/User/Token_openAction.class.php
@@ -4,18 +4,25 @@ class Token_openAction extends UserAction{
 	public function add(){
 		$fun=M('Function')->where(array('id'=>$this->_get('id')))->find();
 		$openwhere=array('uid'=>session('uid'),'token'=>session('token'));
+		$open=M('Token_open')->where($openwhere)->find();
 		//删除掉重复的token
 		$deleteWhere=array();
 		$deleteWhere['uid']=array('neq',session('uid'));
 		$deleteWhere['token']=session('token');
 		M('Token_open')->where($deleteWhere)->delete();
-		$open=M('Token_open')->where($openwhere)->find();		
+		
 		$str['queryname']=str_replace(',,',',',$open['queryname'].','.$fun['funname']);		
 		$back=M('Token_open')->where($openwhere)->save($str);
 		if($back){
 			echo 1;
 		}else{
-			$ins = M('Token_open')->add(array('uid'=>session('uid'), 'token'=>session('token'), 'queryname'=>$fun['funname']));
+			$allfun=M('Function')->select();
+			$queryname = '';
+			foreach ($allfun as $value){
+				$queryname .= $value['funname'].',';
+			}
+			$queryname=rtrim($queryname,',');
+			$ins = M('Token_open')->add(array('uid'=>session('uid'), 'token'=>session('token'), 'queryname'=>$queryname));
 			if ($ins)
 				echo 1;
 			else
@@ -33,6 +40,7 @@ class Token_openAction extends UserAction{
 		$deleteWhere['uid']=array('neq',session('uid'));
 		$deleteWhere['token']=session('token');
 		M('Token_open')->where($deleteWhere)->delete();
+		
 		$str['queryname']=ltrim(str_replace(',,',',',str_replace($fun['funname'],'',$open['queryname'])),',');	
 		$back=M('Token_open')->where($openwhere)->save($str);
 		if($back){
