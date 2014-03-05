dc51a0f6e771310413f92656e6f5e76269651ae1
polo2013 <paul.zhou@163.com>
2014-03-05 22:59:30 +0800

    添加微信支付维权页面、告警页面

---
+++ SRC/weTall/app/Lib/Action/home/alarmAction.class.php
@@ -0,0 +1,9 @@
+<?php
+class indexAction extends frontendAction {
+    public function index() {
+    	
+        //$this->display();
+    	$this->show('微信支付告警页面!');
+    }
+}
+?>
---
+++ SRC/weTall/app/Lib/Action/home/rightsAction.class.php
@@ -0,0 +1,9 @@
+<?php
+class indexAction extends frontendAction {
+    public function index() {
+    	
+        //$this->display();
+    	$this->show('微信支付维权页面!');
+    }
+}
+?>
