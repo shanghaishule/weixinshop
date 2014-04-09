538887c976e6199ec64a9dab645021e3fdbbf19e
polo2013 <paul.zhou@163.com>
2014-04-09 21:30:22 +0800

    告警

---
+++ SRC/weTall/app/Lib/Action/admin/wxpay_alarmAction.class.php
@@ -0,0 +1,26 @@
+<?php
+class wxpay_alarmAction extends backendAction {
+	public function _initialize() {
+		parent::_initialize();
+		$this->_mod = M('wxpay_alarm');
+	}
+	
+	
+	protected function _search() {
+		$map = array();
+		//只能看到自己的
+		$tokenTall = $this->getTokenTall();
+		$me = M('wxpay')->where(array('tokenTall'=>$tokenTall))->find();
+		$map['appid'] = array('eq', $me['appId']);
+		
+		($errortype = $this->_request('errortype', 'trim')) && $map['errortype'] = array('like', '%'.$errortype.'%');
+		$this->assign('search', array(
+				'errortype' => $errortype,
+		));
+		return $map;
+	}
+	
+
+   
+}
+?>
